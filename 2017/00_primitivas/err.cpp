bb215227b06ddbf958cb0ec37e6169d4e6907069
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2017-11-07 11:33:01 +0100

    operandonoviembre

---
+++ 2017/00_primitivas/err.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    fprintf(stderr, "stderr: the world is a vampire.\n");
+
+    return EXIT_SUCCESS;
+}
---
+++ 2017/00_primitivas/javascript/javascript/main.js
@@ -0,0 +1,5 @@
+function main() {
+    var stdout = document.getElementById("stdout")
+    stdout.innerHTML += "stdout:  the world is a vampire"
+    alert("stderr: the world is vampire.")
+}
