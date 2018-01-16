0b57d084169445e665daae8fe4cd5375b2d98015
sergiosanzllamas <sanzsergio4ab@gmail.com>
2018-01-16 17:22:23 +0100

    cuadrado

---
+++ 2017/09_iterativas/simbolos.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	for (int f=0; f<5; f++)
+        printf("*");
+printf("\n");
+
+		for(int c=0; c<5; c++)
+			printf("*");
+        	printf("\n");
+
+       for(int c=0; c<5; c++)
+            printf("*");
+            printf("\n");
+
+for(int c=0; c<5; c++)
+            printf("*");
+            printf("\n");
+
+
+	return EXIT_SUCCESS;
+}
