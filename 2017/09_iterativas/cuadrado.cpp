70a5f5c9bd780762ee8196ed93a4bb4170a7554d
sergiosanzllamas <sanzsergio4ab@gmail.com>
2018-01-17 20:55:49 +0100

    dado 5

--- 2017/09_iterativas/cuadrado.cpp
+++ 2017/09_iterativas/cuadrado.cpp
@@ -2,15 +2,20 @@
 #include <stdlib.h>
 
 int main(){
-    for (int i=0; i<7; i++)
-        printf("* ");
-    printf("\n");
-    for (int i=0;i<1;i++)
-        for(int d=0; d<5;d++)
-            printf("*           *\n");
-    for (int u=0; u<7; u++)
-        printf("* ");
-    printf("\n");
+	for(int y=0; y<4; y++){
 
-    return EXIT_SUCCESS;
+        printf("cuadrado numero %i \n", y);
+		for (int i=0; i<7; i++)
+			printf("* ");
+		printf("\n");
+		for (int i=0;i<1;i++)
+			for(int d=0; d<5;d++)
+				printf("*           *\n");
+		for (int u=0; u<7; u++)
+			printf("* ");
+		printf("\n");
+
+		printf("\n");
+	}
+	return EXIT_SUCCESS;
 }
---
+++ 2017/09_iterativas/cuadrados.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+
+int main(){
+
+	for (int m=0; m<2; m++){
+		for (int e=0; e<3; e++){
+
+			for (int a=0; a<2; a++){
+				printf("****                           ");
+			}
+
+
+			printf("\n");
+
+
+
+		}
+		printf("\n");
+
+		      
+
+		if(m==0){
+
+			for (int j=0; j<3; j++){
+				printf("             ****\n");
+
+			}
+		}
+	}
+
+
+
+	return EXIT_SUCCESS;
+}
