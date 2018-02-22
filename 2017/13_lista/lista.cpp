cbfa356c838efe75fd33eaec823aa5dcc4bba46c
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-02-22 11:16:52 +0100

    trasalante

---
+++ 2017/13_lista/lista.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 0x200
+int main(){
+
+    char **lista = NULL;//PUNTEROS IGUALES A NULL
+    char buffer[N];
+    while(cuando_quiera){
+        printf("dime tu nombre: ");
+
+    }
+	return EXIT_SUCCESS;
+}
