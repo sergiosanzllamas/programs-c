f9c62c7949644c108248f3d35da906cb484886a2
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-02-23 14:12:44 +0100

    vocal

--- 2017/13_lista/lista.cpp
+++ 2017/13_lista/lista.cpp
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include<strings.h>
 #define N 0x200
 int main(){
 
     char **lista = NULL;//PUNTEROS IGUALES A NULL
     char buffer[N];
+    int len;
     while(cuando_quiera){
         printf("dime tu nombre: ");
-
+fgets(buffer, N, stdin);
+len = strlen(buffer);
+algun_stio = (char *) malloc(len * sizeof(char));
+strncpy(algun_stio, buffer, len-1);
+algun_stio[len-1] = '\0';
+   
     }
 	return EXIT_SUCCESS;
 }
