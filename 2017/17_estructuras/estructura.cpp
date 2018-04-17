b8c0689b8801c555a67ba53276cc29819c904101
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-04-17 11:17:06 +0200

    jdfjd

--- 2017/17_estructuras/estructura.cpp
+++ 2017/17_estructuras/estructura.cpp
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+struct Coordenada{
+    double x;
+    double y;
+};
 int main(){
-
+int entero;
+struct Coordenada nave = { 3., 5. };
+nave.x = 3.7;
 	return EXIT_SUCCESS;
 }
