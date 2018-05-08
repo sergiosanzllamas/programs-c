1b9014bbec312ae3fcb66fdb7289a62c886bb5a2
sergiosanzllamas <sanzsergio4ab@gmail.com>
2018-05-08 18:18:53 +0200

    jdjd

--- 2017/23_funcpunto/inter.cpp
+++ 2017/23_funcpunto/inter.cpp
@@ -1,10 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#define paso 0.05
-double parabola(double r){
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+#define paso 0.00005
+
+double parabola(double x){
+
+return x * x;
+}
+
+double rec(double x){
+
+
+return 3 * x + 4;
+}
+
+double e(double x){
+
+return exp(1 / x);
+}
+
+double inter(double li, double ls, double (*f)(double)){
+int res = 0;
+for( double x=li; x<=ls; x += paso)
+	res += (*f)(x);
+return res * paso;
 
 }
 int main(){
 
-	return EXIT_SUCCESS;
+
+
+
+
+
+printf("la integral es %.2lf", inter(0., 4., &parabola));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return EXIT_SUCCESS;
 }
